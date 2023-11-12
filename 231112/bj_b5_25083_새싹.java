3208e4a44e3b783b4d08498e11650e33bbfb9210
Hyunji Yoo <ccamy0915@naver.com>
2023-11-12 23:47:47 +0900

    1 commit

---
+++ 231112/bj_b5_25083_새싹.java
@@ -0,0 +1,15 @@
+package etc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_b5_25083_새싹 {
+	public static void main(String[] args) throws Exception{
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
