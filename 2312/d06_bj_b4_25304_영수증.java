b3f487ac18ab5fab0c53fb74e449347ed4986212
련지 <88645538+YooHyunJi@users.noreply.github.com>
2023-12-06 15:39:06 +0900

    1 commit

--- 2312/d06_bj_b4_25304_영수증.java
+++ 2312/d06_bj_b4_25304_영수증.java
@@ -4,14 +4,14 @@ import java.io.*;
 public class d06_bj_b4_25304_영수증 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        int total = Integer.parseInt(br.readLine());
-        int quan = Integer.parseInt(br.readLine());
-        int real = 0;
-        for(int q=0; q<quan; q++){
-            StringTokenizer st= new StringTokenizer(br.readLine());
-            real += Integer.parseInt(st.nextToken())*Integer.parseInt(st.nextToken());
-        }
-        if(total == real) System.out.println("Yes");
-        else System.out.println("No");
-    }
+	        int total = Integer.parseInt(br.readLine());
+	        int quan = Integer.parseInt(br.readLine());
+	        int real = 0;
+	        for(int q=0; q<quan; q++){
+	            StringTokenizer st= new StringTokenizer(br.readLine());
+	            real += Integer.parseInt(st.nextToken())*Integer.parseInt(st.nextToken());
+	        }
+	        if(total == real) System.out.println("Yes");
+	        else System.out.println("No");
+	}
 }
