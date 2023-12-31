44f722ce0e5ff6669de616ec9f8a6fad5c8be23e
련지 <88645538+YooHyunJi@users.noreply.github.com>
2023-12-31 16:39:12 +0900

    1 commit

---
+++ 2312/d31_bj_b5_코드마스터2023.java
@@ -0,0 +1,24 @@
+package a2312;
+
+import java.io.*;
+import java.util.*;
+
+public class d31_bj_b5_코드마스터2023 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        switch (br.readLine()){
+            case "SONGDO":
+                System.out.println("HIGHSCHOOL");
+                break;
+            case "CODE":
+                System.out.println("MASTER");
+                break;
+            case "2023":
+                System.out.println("0611");
+                break;
+            case "ALGORITHM":
+                System.out.println("CONTEST");
+                break;
+        }
+    }
+}
