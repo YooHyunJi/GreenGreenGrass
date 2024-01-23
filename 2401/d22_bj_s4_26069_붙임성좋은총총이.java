f9c3434e30125d4ecdd1b28f07e1aea472945705
유현지 <88645538+YooHyunJi@users.noreply.github.com>
2024-01-23 21:00:56 +0900

    1 commit

---
+++ 2401/d22_bj_s4_26069_붙임성좋은총총이.java
@@ -0,0 +1,21 @@
+package a2401;
+
+import java.io.*;
+import java.util.*;
+
+public class d22_bj_s4_26069_붙임성좋은총총이 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<String> list = new ArrayList<>();
+        list.add("ChongChong");
+        int N = Integer.parseInt(br.readLine());
+        for(int n=0; n<N; n++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String A = st.nextToken();
+            String B = st.nextToken();
+            if(list.contains(A) && !list.contains(B)) { list.add(B); }
+            else if (!list.contains(A) && list.contains(B)) { list.add(A); }
+        }
+        System.out.println(list.size());
+    }
+}
