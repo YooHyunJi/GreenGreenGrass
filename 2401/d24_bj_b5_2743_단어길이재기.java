7afe5a126dbeb38bd234cbf306de8a7d952cd3fc
유현지 <88645538+YooHyunJi@users.noreply.github.com>
2024-01-24 21:11:22 +0900

    1 commit

---
+++ 2401/d24_bj_b5_2743_단어길이재기.java
@@ -0,0 +1,10 @@
+package a2401;
+
+import java.io.*;
+
+public class d24_bj_b5_2743_단어길이재기 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length());
+    }
+}
