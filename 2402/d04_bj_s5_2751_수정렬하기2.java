287753b7ee5e22adf5f6d5fb210ed9c62d8bfb7a
유현지 <88645538+YooHyunJi@users.noreply.github.com>
2024-02-05 08:48:28 +0900

    2 commit

---
+++ 2402/d04_bj_s5_2751_수정렬하기2.java
@@ -0,0 +1,16 @@
+package a2402;
+
+import java.io.*;
+import java.util.*;
+
+public class d04_bj_s5_2751_수정렬하기2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int N = Integer.parseInt(br.readLine());
+        for(int n=0; n<N; n++){ pq.offer(Integer.parseInt(br.readLine())); }
+        for(int n=0; n<N; n++){ sb.append(pq.poll()).append("\n"); }
+        System.out.println(sb);
+    }
+}
