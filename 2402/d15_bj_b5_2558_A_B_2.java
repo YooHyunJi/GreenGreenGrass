884987228af853ffd93310312191d21354906a3b
유현지 <88645538+YooHyunJi@users.noreply.github.com>
2024-02-15 22:01:04 +0900

    1 commit

---
+++ 2402/d15_bj_b5_2558_A_B_2.java
@@ -0,0 +1,11 @@
+package a2402;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class d15_bj_b5_2558_A_B_2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine())+Integer.parseInt(br.readLine()));
+    }
+}
