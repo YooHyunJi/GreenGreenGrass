ece5482ffa617542c303a23b7700d011e5817723
유현지 <88645538+YooHyunJi@users.noreply.github.com>
2024-02-24 23:13:38 +0900

    1 commit

---
+++ 2402/d24_bj_b5_2420_사파리월드.java
@@ -0,0 +1,12 @@
+package a2402;
+
+import java.io.*;
+import java.util.*;
+
+public class d24_bj_b5_2420_사파리월드 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println(Math.abs(Long.parseLong(st.nextToken())-Long.parseLong(st.nextToken())));
+    }
+}
