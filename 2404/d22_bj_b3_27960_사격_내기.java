2a402f03dfbd48c65c2058f8422fd07817ea0414
유현지 <ccamy0915@naver.com>
2024-04-22 08:43:33 +0900

    1 commit

--- 2404/d22_bj_b3_27960_사격_내기.java
+++ 2404/d22_bj_b3_27960_사격_내기.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class d22_bj_b3_27960_사격_내기 {
     public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
