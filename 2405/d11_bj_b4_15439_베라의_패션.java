00765e43fbc102b3de71240bee5b39d0f0c0374f
유현지 <88645538+YooHyunJi@users.noreply.github.com>
2024-05-11 22:29:10 +0900

    2 commit

---
+++ 2405/d11_bj_b4_15439_베라의_패션.java
@@ -0,0 +1,11 @@
+package a2405;
+
+import java.io.*;
+
+public class d11_bj_b4_15439_베라의_패션 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N * (N-1));
+    }
+}
