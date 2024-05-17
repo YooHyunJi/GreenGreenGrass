1b2e4bf1c9f65fb7b13a12a54a1122f8e1fe4434
련지 <88645538+YooHyunJi@users.noreply.github.com>
2024-05-17 21:42:40 +0900

    1 commit

---
+++ 2405/d17_bj_b5_5337_웰컴.java
@@ -0,0 +1,9 @@
+package a2405;
+
+public class d17_bj_b5_5337_웰컴 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
