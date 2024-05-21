e06f99470d428f75121abcba451f0e182395101b
유현지 <88645538+YooHyunJi@users.noreply.github.com>
2024-05-21 22:15:42 +0900

    1 commit

---
+++ 2405/d21_bj_b5_16394_홍익대학교.java
@@ -0,0 +1,11 @@
+package a2405;
+
+import java.io.*;
+
+public class d21_bj_b5_16394_홍익대학교 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N-1946);
+    }
+}
