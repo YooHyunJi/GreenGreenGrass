9d850a2223a2ad4685dc7965e73bd7cfe4b896ed
유현지 <ccamy0915@naver.com>
2024-09-11 22:18:01 +0900

    1 commit

---
+++ 2409/d11_bj_b5_5339_콜센터.java
@@ -0,0 +1,7 @@
+package a2409;
+
+public class d11_bj_b5_5339_콜센터 {
+    public static void main(String[] args) {
+        System.out.println("     /~\\\n    ( oo|\n    _\\=/_\n   /  _  \\\n  //|/.\\|\\\\\n ||  \\ /  ||\n============\n|          |\n|          |\n|          |");
+    }
+}
