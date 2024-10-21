06c0b34e00484ceeb9d569cc42b0eafff6d78285
련지 <88645538+YooHyunJi@users.noreply.github.com>
2024-10-21 22:00:01 +0900

    1 commit

---
+++ 2410/d21_bj_b5_15733_나는_누구인가.java
@@ -0,0 +1,9 @@
+package a2410;
+
+import java.io.*;
+
+public class d21_bj_b5_15733_나는_누구인가 {
+    public static void main(String[] args) throws Exception{
+        System.out.println("I'm Sexy");
+    }
+}
