52520f2ff490a66f2ebcbe5e8b11a00c1b252a28
련지 <88645538+YooHyunJi@users.noreply.github.com>
2024-10-29 22:00:01 +0900

    1 commit

---
+++ 2410/d30_bj_s5_5555_반지.java
@@ -0,0 +1,20 @@
+package a2410;
+
+import java.io.*;
+
+public class d30_bj_s5_5555_반지 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String target = br.readLine();
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+        for(int n=0; n<N; n++){
+            String line = br.readLine();
+            line = line.concat(line);
+            if(line.contains(target)){
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
