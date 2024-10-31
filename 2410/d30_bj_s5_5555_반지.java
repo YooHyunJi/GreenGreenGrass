326142a8b2da4de2044a8731282661adb3ccd5e6
유현지 <ccamy0915@naver.com>
2024-10-31 21:54:40 +0900

    1 commit

--- 2410/d30_bj_s5_5555_반지.java
+++ 2410/d30_bj_s5_5555_반지.java
@@ -3,15 +3,15 @@ package a2410;
 import java.io.*;
 
 public class d30_bj_s5_5555_반지 {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String target = br.readLine();
         int N = Integer.parseInt(br.readLine());
         int answer = 0;
-        for(int n=0; n<N; n++){
+        for (int n = 0; n < N; n++) {
             String line = br.readLine();
             line = line.concat(line);
-            if(line.contains(target)){
+            if (line.contains(target)) {
                 answer++;
             }
         }
--- 2410/d31_bj_b4_9316_Hello_Judge.java
+++ 2410/d31_bj_b4_9316_Hello_Judge.java
@@ -1,13 +1,14 @@
+package a2410;
+
 import java.io.*;
-import java.util.*;
 
 public class d31_bj_b4_9316_Hello_Judge {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        for(int n=1; n<=N; n++){
-          sb.append("Hello World, Judge ").append(n).append("!\n");
+        for (int n = 1; n <= N; n++) {
+            sb.append("Hello World, Judge ").append(n).append("!\n");
         }
         System.out.println(sb);
     }
